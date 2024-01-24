2f277fcbb2ee5286bb525ab2e46b4778823ed675
lindaweb92 <148793222+lindaweb92@users.noreply.github.com>
2024-01-24 16:42:42 +0100

    progetto 2 - job-redux

--- job-redux/src/App.jsx
+++ job-redux/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// react components.
+import MainLayout from "./layouts/MainLayout";
 
+// pages.
+import Add from "./pages/Add";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+
+// redux store.
+import store from "./redux/app/store";
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Provider store={store}>
+      <Router>
+        <MainLayout>
+          <Routes>
+            <Route path="" element={<Navigate to="jobs/all" />} />
+            <Route path="jobs" element={<Navigate to="all" />} />
+            <Route path="jobs/add" element={<Add />} />
+            <Route path="jobs/edit/:jobId" element={<Edit />} />
+            <Route path="jobs/:type" element={<Home />} />
+          </Routes>
+        </MainLayout>
+      </Router>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
