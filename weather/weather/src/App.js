94769745f7ba45ecb5c3369c46fc3b562aa5821e
lindaweb92 <148793222+lindaweb92@users.noreply.github.com>
2024-01-26 17:18:59 +0100

    progetto 1 weather

---
+++ weather/weather/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+        <CurrentLocation />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ weather/weather/src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "21efcd4989131d7914087360ad224c2a",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
